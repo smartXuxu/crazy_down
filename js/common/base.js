6e4cae2a67f1801a28a173d21cfc263818cc2a03
smartXu <1527196854@qq.com>
2019-01-07 21:18:44 +0800

    支付宝支付完成

--- js/common/base.js
+++ js/common/base.js
@@ -32,7 +32,7 @@ if (window.history && window.history.pushState) {
 //var baseUrl="http://39.104.127.252:8080/dongdong/";
 //var baseUrl='http://192.168.1.181:8087' 海波;
 //var baseUrl='http://39.104.127.252:8087';
-var baseUrl='http://x384gw.natappfree.cc';
+var baseUrl='http://vckvvv.natappfree.cc';
 plus.screen.lockOrientation('portrait-primary');
 //锁死屏幕方向为竖屏
 function back(){

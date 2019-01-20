e6e2b8ac6b0d64d371a3688c17fc0a6f0c3b00c1
smartXu <1527196854@qq.com>
2019-01-20 22:21:55 +0800

    1.1.0

--- js/common/base.js
+++ js/common/base.js
@@ -32,8 +32,9 @@ if (window.history && window.history.pushState) {
 //var baseUrl="http://39.104.127.252:8080/dongdong/";
 //var baseUrl='http://192.168.1.181:8087' 海波;
 //var baseUrl='http://39.104.127.252:8087';
-var baseUrl='http://vckvvv.natappfree.cc';
-plus.screen.lockOrientation('portrait-primary');
+//var baseUrl='http://39.104.127.252:8080/fkdp';
+var baseUrl='http://39.104.127.252:8181/fkdp';
+//plus.screen.lockOrientation('portrait-primary');
 //锁死屏幕方向为竖屏
 function back(){
     history.go(-1);
